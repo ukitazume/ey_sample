47568501072cd1c9f2f4480a14220dec3e4527b8
Yu Kitazume <u.kitazume@gmail.com>
2013-06-17 21:32:51 +0900

    json version

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'json', "1.8.0"
 require 'tinder'
 
 campfire = Tinder::Campfire.new '473d191d', :token => '8bdd447ce9660a31941922d48ab4862807db49c5'
