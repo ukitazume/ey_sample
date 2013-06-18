ed86bda9e4e6bf00c9b6449a1a483d57f1d46b2b
Yu Kitazume <u.kitazume@gmail.com>
2013-06-18 18:19:23 +0900

    direct db

--- config/initializers/mongo_hg.rb
+++ config/initializers/mongo_hg.rb
@@ -1,8 +1,8 @@
+case Rails.env
+db_name = "#{Rails.application.class.parent_name.underscore}_#{Rails.env}"
 mongohq_connection_uri = case Rails.env
            when 'production' then EY::Config.get(:mongohq, 'MONGOHQ_URL')
            when 'staging' then EY::Config.get(:mongohq, 'MONGOHQ_URL')
-           else 'mongodb://localhost:27017'
+           else "mongodb://localhost:27017/#{db_name}"
            end
-$mongo_conn = Mongo::Connection.from_uri mongohq_connection_uri
-db_name = "#{Rails.application.class.parent_name.underscore}_#{Rails.env}"
-$mongo_db = $mongo_conn[db_name]
+$mongo_db = Mongo::Connection.from_uri mongohq_connection_uri
