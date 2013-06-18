6920750fd3e378c588ec738cf52cf3ede86714ee
Yu Kitazume <u.kitazume@gmail.com>
2013-06-18 09:15:43 +0900

    comment out

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,9 +1,9 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'json', "1.8.0"
-require 'tinder'
-
-campfire = Tinder::Campfire.new '473d191d', :token => '8bdd447ce9660a31941922d48ab4862807db49c5'
-
-room = campfire.find_room_by_id '566129'
-room.speak "#{revision} is deployed #{environment_name} by #{deployed_by}."
+# require 'rubygems'
+# require 'bundler/setup'
+# require 'json', "1.8.0"
+# require 'tinder'
+# 
+# campfire = Tinder::Campfire.new '473d191d', :token => '8bdd447ce9660a31941922d48ab4862807db49c5'
+# 
+# room = campfire.find_room_by_id '566129'
+# room.speak "#{revision} is deployed #{environment_name} by #{deployed_by}."
