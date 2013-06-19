5aaf6bc992f357fdaf9209898ffa9b5c0fd8ee9d
Yu Kitazume <u.kitazume@gmail.com>
2013-06-19 16:37:39 +0900

    ...

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,5 @@
 CLOUDINARY_URL = case Rails.env
-                 when 'production', 'staging' then EY::Config.get('cloudinary_url', 'CLOUDINARY_URL')
+                 when 'production', 'staging' then EY::Config.get('cloudinary', 'CLOUDINARY_URL')
                  else ''
                  end
 Cloudinary.config.cloud_name = CLOUDINARY_URL.split('@').last rescue nil
