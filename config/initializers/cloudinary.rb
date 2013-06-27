7a5c608ceb118631937c2fabd707320adcce98ec
Yu Kitazume <u.kitazume@gmail.com>
2013-06-27 12:28:24 +0900

    remove cloudinary

--- config/initializers/cloudinary.rb
+++
@@ -1,5 +0,0 @@
-CLOUDINARY_URL = case Rails.env
-                 when 'production', 'staging' then EY::Config.get('cloudinary', 'CLOUDINARY_URL')
-                 else ''
-                 end
-# Cloudinary.config.cloud_name = CLOUDINARY_URL.split('@').last rescue nil
