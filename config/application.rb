fa52aabd5919ab7304668469dcc92bf100ccf168
Yu Kitazume <u.kitazume@gmail.com>
2013-07-24 13:01:43 +0900

    change assets config at ey

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module EySample
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += Dir["#{Rails.root}/app/models/**/"]
-    config.assets.enabled = false
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
