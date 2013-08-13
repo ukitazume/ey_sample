285c8d4b3ca0d6534add4f80d9e925d5a6a1ddbd
Yu Kitazume <u.kitazume@gmail.com>
2013-08-13 11:56:45 +0900

    remove test multi bite

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module EySample
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # 日本語
     # config.i18n.default_locale = :de
   end
 end
