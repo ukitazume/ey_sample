230caf1810d366b0b9b79af896e81b860b16fd0d
Yu Kitazume <u.kitazume@gmail.com>
2013-08-13 10:28:54 +0900

    utf-8 for test

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module EySample
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # 日本語
     # config.i18n.default_locale = :de
   end
 end
