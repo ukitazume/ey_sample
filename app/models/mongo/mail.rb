b773bff3825dba5aae1950dabff0d69ed55ebb95
Yu Kitazume <u.kitazume@gmail.com>
2013-09-18 14:00:42 +0900

    return mongo

--- app/models/mongo/mail.rb
+++ app/models/mongo/mail.rb
@@ -1,15 +1,15 @@
-# class Mail
-#   @@coll = $mongo_db[self.name.underscore.pluralize]
-# 
-#   def self.set(body)
-#     @@coll.insert({body: body, created_at: Time.now})
-#   end
-# 
-#   def self.tail
-#     @@coll.find({}, {:limit => 20, :sort => ['created_at', Mongo::DESCENDING]})
-#   end
-# 
-#   def self.count
-#     @@coll.count rescue 0
-#   end
-# end
+class Mail
+  @@coll = $mongo_db[self.name.underscore.pluralize]
+
+  def self.set(body)
+    @@coll.insert({body: body, created_at: Time.now})
+  end
+
+  def self.tail
+    @@coll.find({}, {:limit => 20, :sort => ['created_at', Mongo::DESCENDING]})
+  end
+
+  def self.count
+    @@coll.count rescue 0
+  end
+end
