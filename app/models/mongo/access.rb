48bedbe3a01ada22dd98f073bc3a4dfd815b1103
Yu Kitazume <u.kitazume@gmail.com>
2013-09-18 10:42:50 +0900

    for mongo test

--- app/models/mongo/access.rb
+++ app/models/mongo/access.rb
@@ -1,15 +1,15 @@
-class Access
-  @@coll = $mongo_db[self.name.underscore.pluralize]
-
-  def self.set(request)
-    @@coll.insert({remote_host: request.env['REMOTE_HOST'], created_at: Time.now})
-  end
-
-  def self.tail
-    @@coll.find({}, {:limit => 20, :sort => ['created_at', Mongo::DESCENDING]})
-  end
-
-  def self.count
-    @@coll.count rescue 0
-  end
-end
+# class Access
+#   @@coll = $mongo_db[self.name.underscore.pluralize]
+# 
+#   def self.set(request)
+#     @@coll.insert({remote_host: request.env['REMOTE_HOST'], created_at: Time.now})
+#   end
+# 
+#   def self.tail
+#     @@coll.find({}, {:limit => 20, :sort => ['created_at', Mongo::DESCENDING]})
+#   end
+# 
+#   def self.count
+#     @@coll.count rescue 0
+#   end
+# end
