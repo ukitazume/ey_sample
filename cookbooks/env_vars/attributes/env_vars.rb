03f8ca2611b35327dbc9822f5feec3eb250e993c
Yu Kitazume <u.kitazume@gmail.com>
2013-09-18 14:05:54 +0900

    add cookbook

---
+++ cookbooks/env_vars/attributes/env_vars.rb
@@ -0,0 +1,7 @@
+# Specify environment variables for Unicorn or Passenger here
+#
+# The example below will tune garbage collection for REE and Ruby 1.9.x and higher 
+ 
+default[:env_vars] = {
+  :HOGE => 'test'
+}
---
+++ cookbooks/ey_sample/recipes/default.rb
@@ -0,0 +1,4 @@
+#
+# Cookbook Name:: ey_sample
+# Recipe:: default
+#
