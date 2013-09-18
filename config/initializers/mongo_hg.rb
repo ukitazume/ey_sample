0e1c87c9dfebfd643b27a0b4b687a120f4a1ea88
Yu Kitazume <u.kitazume@gmail.com>
2013-09-18 10:37:45 +0900

    for mongo test

--- config/initializers/mongo_hg.rb
+++ config/initializers/mongo_hg.rb
@@ -1,9 +1,9 @@
 db_name = "#{Rails.application.class.parent_name.underscore}_#{Rails.env}"
 mongohq_connection_uri = case Rails.env
-           when 'production' then EY::Config.get(:mongohq, 'MONGOHQ_URL')
-           when 'staging' then EY::Config.get(:mongohq, 'MONGOHQ_URL')
-           else "mongodb://localhost:27017/#{db_name}"
-           end
+                         when 'production' then EY::Config.get(:mongohq, 'MONGOHQ_URL') rescue nil
+                         when 'staging' then EY::Config.get(:mongohq, 'MONGOHQ_URL') rescue nil
+                         else "mongodb://localhost:27017/#{db_name}"
+                         end
 # default connect test db,,,
 db_name = mongohq_connection_uri.split('/').last
 $mongo_client = Mongo::Connection.from_uri mongohq_connection_uri
