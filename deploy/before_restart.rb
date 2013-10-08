3fddf681b596a0381ccf53255dde37aa9c9857d2
Yu Kitazume <u.kitazume@gmail.com>
2013-10-08 14:36:24 +0900

    remove change unicorn

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,1 @@
-run "cp #{config.release_path}/config/unicorn.rb #{config.shared_path}/config/custom_unicorn.rb"
+#run "cp #{config.release_path}/config/unicorn.rb #{config.shared_path}/config/custom_unicorn.rb"
