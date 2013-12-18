2296088e3260497a40ba59edef90be169dfb113d
Yu Kitazume <u.kitazume@gmail.com>
2013-12-18 17:29:41 +0900

    add deploy hook comment

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,4 @@
+info "-------------------------"
+info "Infomation at deploy hook"
+info "-------------------------"
 #run "cp #{config.release_path}/config/unicorn.rb #{config.shared_path}/config/custom_unicorn.rb"
